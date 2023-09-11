1e304c250e537e0851415b8cb47ba163b53d1e33
sagomezmartinez <sagomezmartinez@USQROSAGOMEZMA1.us.deloitte.com>
2023-09-11 08:33:35 -0500

    JUnit5 - Second Commit

--- src/test/java/guru/springframework/GreetingTest.java
+++ src/test/java/guru/springframework/GreetingTest.java
@@ -1,11 +1,19 @@
 package guru.springframework;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class GreetingTest {
 
 	private Greeting greeting;
+	
+	@BeforeAll
+	public static void beforeClass() {
+		System.out.println("Before - I am only calles once!!!");
+	}
 	
 	@BeforeEach
 	void setUp() {
@@ -21,5 +29,20 @@ public class GreetingTest {
 	@Test
 	void helloWorld1() {
 		System.out.println(greeting.helloWorld("John"));
+	}
+	
+	@Test
+	void helloWorld2() {
+		System.out.println(greeting.helloWorld("Sam"));
+	}
+	
+	@AfterEach
+	void tearDown() {
+		System.out.println("In After Each...");
+	}
+	
+	@AfterAll
+	public static void afterClass() {
+		System.out.println("After!!! ***** - I am only called once!!!");
 	}
 }
